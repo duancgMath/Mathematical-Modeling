6da96a8a70149888fe85a1755628c818e947f22d
duancgMath <duan_cg@aliyun.com>
2018-11-29 23:37:30 +0800

    Update main.m

--- demo/code/example04/main.m
+++ demo/code/example04/main.m
@@ -1,40 +1,27 @@
 
 clear, clc
-%% Before cutting down trees
-clear, clc
 
-A = [0 0 0.33;
-     0.30 0 0;
-     0 0.71 0.94];
-x = [20;20;20];
-
-X = [];
-for k = 1:200
-  X = [X, x];
-  x = A*x;
-end
+%% Image compress based on SVD
+A = imread('lena.bmp');
+A = rgb2gray(A);
+A = im2double(A);
+[u, s, v] = svd(A);
 
 figure
-comet3(X(1,:),X(2,:),X(3,:)) 
-plot3(X(1,:),X(2,:),X(3,:),'-o')
-grid on
-xlabel('I')
-ylabel('II')
-zlabel('III')
+imshow(A)
+title('Image')
 
-%% After cutting down trees
-clear, clc
-
-A = [0 0 0.33;
-     0.18 0 0;
-     0 0.71 0.94];
-x = [20;20;20];
-
-X = [];
-for k = 1:200
-  X = [X, x];
-  x = A*x;
+ss = zeros(size(s));
+for alpha = 0.05:0.05:0.30
+  k = floor(size(s,1)*alpha);
+  ss(1:k,1:k) = s(1:k,1:k);
+  ratio = sum(diag(ss))/sum(diag(s));
+  A = u*ss*v';
+  figure
+  imshow(A)
+  title(['alpha=',num2str(alpha),' ratio=',num2str(ratio)])
 end
+
 
 figure
 comet3(X(1,:),X(2,:),X(3,:)) 
